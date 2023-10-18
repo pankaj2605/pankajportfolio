1ff2458c9892a8329fce59e2e191beeafa5a004a
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-18 23:00:10 +0530

    change founder

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -60,7 +60,7 @@ const Resume = () => {
               {/* design */}
               <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
                 <h1 className="text-[1.4rem] font-semibold sm:text-xl">
-                  CTO
+                  Founder
                 </h1>
                 <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
                   Jai Guru Sai Doors
