bb47370c94709d3ee28ad74342db6e5a09e77b2b
Pankaj Kumar <pankajs2167@gmal.com>
2024-04-26 21:38:18 +0530

    updated resume 2

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
-import resumePDF from "../../assets/Pankaj_Kumar.pdf";
+import resumePDF from "../../assets/PANKAJ_KUMAR.pdf";
 import { Link } from "react-router-dom";
 
 
